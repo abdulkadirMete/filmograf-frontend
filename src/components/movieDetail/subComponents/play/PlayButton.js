f9c641c1622502cf1bdd295110f6a76214618714
Kadir Mete <abdulkadir.mete@bil.omu.edu.tr>
2022-04-10 23:38:31 +0300

    .

--- src/components/movieDetail/subComponents/play/PlayButton.js
+++ src/components/movieDetail/subComponents/play/PlayButton.js
@@ -6,31 +6,28 @@ import { PlayButtonContainer } from "./PlayButtonStyles";
 import { toast } from "react-toastify";
 
 export const PlayButton = ({ video, increaseMovieView }) => {
-  const { increaseViewLoading, increaseViewSuccess, increaseViewError } =
-    useContext(MovieContext);
-  const [redirectLock, setRedirectLock] = useState(false);
+  // const { increaseViewLoading, increaseViewSuccess, increaseViewError } =
+  //   useContext(MovieContext);
+  // const [redirectLock, setRedirectLock] = useState(false);
 
-  console.log(increaseViewSuccess);
-  useEffect(() => {
-    console.log("work");
-    if (redirectLock) {
-      if (increaseViewLoading) return;
-      if (increaseViewSuccess) {
-        window.location.href = video;
-      } else {
-        toast.error(strings.defaultError);
-      }
-      setRedirectLock(false);
-    }
-  }, [redirectLock, increaseViewSuccess]);
+  // useEffect(() => {
+  //   console.log("work");
+  //   if (redirectLock) {
+  //     if (increaseViewLoading) return;
+  //     if (increaseViewSuccess) {
+  //     } else {
+  //       toast.error(strings.defaultError);
+  //     }
+  //     setRedirectLock(false);
+  //   }
+  // }, [redirectLock, increaseViewSuccess]);
 
   const handleClick = () => {
-    if (redirectLock) return setRedirectLock(false);
     increaseMovieView();
     if (video.includes(strings.brokenSp)) {
       alert(strings.brokenSpMessage);
     }
-    setRedirectLock(true);
+    window.location.href = video;
   };
   return (
     <PlayButtonContainer onClick={handleClick}>
